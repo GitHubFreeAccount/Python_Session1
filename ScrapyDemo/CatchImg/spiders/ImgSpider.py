4f77a38b502d9bf3be520c83c64b2b9abc4ec4ac
RomaterWoozi <qianshuiqianshui@gmail.com>
2018-12-07 09:59:08 +0800

    爬虫代码更新

--- ScrapyDemo/CatchImg/spiders/ImgSpider.py
+++ ScrapyDemo/CatchImg/spiders/ImgSpider.py
@@ -9,16 +9,15 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = "catchImg"
     start_urls = [
-        'http://quotes.toscrape.com/tag/humor/',
+        'https://mp.weixin.qq.com/s/Gw5g1RXoUvjVU7aSeLsNsQ',
     ]
 
     def parse(self, response):
-        for quote in response.css('div.quote'):
+        for img in response.css('img'):
             yield {
-                'text': quote.css('span.text::text').extract_first(),
-                'author': quote.xpath('span/small/text()').extract_first(),
+                'src': img.css('img::attr(src)').extract_first(),
             }
 
-        next_page = response.css('li.next a::attr("href")').extract_first()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)
+        # next_page = response.css('li.next a::attr("href")').extract_first()
+        # if next_page is not None:
+        #     yield response.follow(next_page, self.parse)
